ab4e92bc607622f7e610b136897d74ffe99c432e
AlbertoMessineo <albevorox@gmail.com>
2023-11-22 17:30:04 +0100

    feat : creazione array

--- _js-snack_1/main.js
+++ _js-snack_1/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// Obj arrays creation
+const bicycles = [
+    {
+        nome: 'Bianchi da corsa velocissima in turbo carbonio',
+        peso_in_kg: '8',
+
+    },
+    {   
+        nome: 'Cube un poco pesantuccia',
+        peso_in_kg: '20',
+    },
+    {   
+        nome: 'Atala in alluminio',
+        peso_in_kg: '16',
+    },
+    {
+        
+        nome: 'Specialized in carbonio',
+        peso_in_kg: '11',
+    },
+    {
+        nome: 'Rose forcella in titanio ',
+        peso_in_kg: '23',
+    },
+];
+// for loop 
+for ( let i = 0, i < bicycles.length - 1, i++){
+const {peso_in_kg} = bicycles;
+console.log({peso_in_kg}[i]);
+}
